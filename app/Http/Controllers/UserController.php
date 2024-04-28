b57df10a9ba6d2bec6bd4f83b5d1c08227fafeb5
Fran <franrocher95@gmail.com>
2024-04-28 12:56:23 +0200

    feat:adding get and update profile controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function getProfile(Request $request)
+    {
+        try {
+            $user = auth()->user();
+
+            if (!$user) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'User not found',
+
+                ], 400);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User retrieved successfully',
+                'data' => $user
+            ], 200);
+
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User cannot be retrieved',
+                'error' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function updateProfile(Request $request){
+        try {
+            $user = User::where("id", auth()->user()->id)
+            ->first();
+
+            if(!$user){
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "user not found"
+                    ],
+                    400
+                );
+            }
+
+            $name = $request->input('name');
+            $biography = $request->input('biography');
+
+            $validator = Validator::make($request->all(), [ //validator facades
+                'name' => 'required|string|min:4|max:10',
+                'biography' => 'required|string|max:30'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    "success" => false,
+                    "message" => "Validation failed",
+                    "errors" => $validator->errors()
+                ],400);
+            }
+
+            if ($name) {
+                $user->name = $name;
+                $user->biography = $biography;
+            }
+
+
+            $user->save();
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "user profile updated successfully",
+                    "data" => $user
+                ],
+                200
+            );
+    
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User cannot be updated',
+                'error' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,14 @@ Route::get('/', function () {
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+
+
+// User 
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+// Your profile
+Route::get('/user/me', [UserController::class, 'getProfile']);
+Route::put('/user/me', [UserController::class, 'updateProfile']);
+
+});
